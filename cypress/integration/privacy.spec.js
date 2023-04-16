b0a8912df4ad8c5de55a44d1dc91b37fd99327dd
MarcosSantosDev <marcosadriano034@gmail.com>
2023-04-16 18:31:35 -0300

    use o metodo times do cypress lodash para repetir um teste por um numero de vezes

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,8 +1,10 @@
-it('testa a página da política de privacidade de forma independente', function() {
-    cy.visit('./src/privacy.html');
-
-    cy.title()
-        .should('equal', 'Central de Atendimento ao Cliente TAT - Política de privacidade')
-
-    cy.contains('Talking About Testing')
+Cypress._.times(5, function() {
+    it('testa a página da política de privacidade de forma independente', function() {
+        cy.visit('./src/privacy.html');
+    
+        cy.title()
+            .should('equal', 'Central de Atendimento ao Cliente TAT - Política de privacidade')
+    
+        cy.contains('Talking About Testing')
+    })
 })
